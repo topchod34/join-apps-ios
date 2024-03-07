2b0705fe0c574f1465c79916b5f5f191b5ce7e92
topchod34 <162571263+topchod34@users.noreply.github.com>
2024-03-07 10:36:07 +0000

    join

---
+++ index.js
@@ -0,0 +1,14 @@
+// Function to generate a random password
+function generatePassword(length) {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    return password;
+}
+
+// Generate a random password of length 10
+const password = generatePassword(10);
+console.log("Random password:", password);
